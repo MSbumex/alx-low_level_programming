38ad1f823ae489a0fadf65d5212943d5c0c6cc58
MSbumex <58666149+MSbumex@users.noreply.github.com>
2023-02-16 14:36:13 -0800

    use the func puts  prints exactly "Programming is like building a multilingual puzzle, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+/* print a string
+ * folowed by newline 
+ * return 0
+ */
+int main(void) 
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
