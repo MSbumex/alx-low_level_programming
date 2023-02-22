a22ca3b11010b64fb73e1ca47227a9e59e65436e
MSbumex <58666149+MSbumex@users.noreply.github.com>
2023-02-22 02:14:00 -0800

    program that prints _putchar, followed by a new line. and main.h file containing all prototype

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef main_h
+#define main_h
+/**
+ * main - declaration for the prototype
+ * This is header file containing prototypes of all functions created in
+ * 0x02-functions_nested_loops dir and tasks and
+ * the prototype of the function _putchar included;
+ */
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+#endif
