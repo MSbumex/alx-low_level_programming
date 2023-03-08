83de480bf8cab5d0ed5f3dbbd56a5a273e886f74
MSbumex <58666149+MSbumex@users.noreply.github.com>
2023-03-08 14:49:06 -0800

    0x08. C - Recursion

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 /**
  * main - declaration for the prototype
  * This is header file containing prototypes of all functions created in
- * 0x06. C - More pointers, arrays and strings  and tasks and
+ * 0x07. C - More pointers, arrays and strings  and tasks and
  * the prototype of the function _putchar included;
  */
 int _putchar(char c);
