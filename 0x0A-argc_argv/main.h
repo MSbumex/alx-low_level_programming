d4c6ed5266676bd8cfccb8accd9eb18650de6e70
MSbumex <58666149+MSbumex@users.noreply.github.com>
2023-03-15 10:17:17 -0700

    0x0B. C - malloc, free

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,7 +3,7 @@
 /**
  * main - declaration for the prototype
  * This is header file containing prototypes of all functions created in
- * 0x08-recursion  and tasks and
+ *  0x0A-argc_argv  and tasks and
  * the prototype of the function _putchar included;
  */
 int _putchar(char c);
