80ffe677f733fa130849682c68764342d1662e57
MSbumex <58666149+MSbumex@users.noreply.github.com>
2023-03-31 22:13:13 -0700

    a 64-bit program in assembly that prints Hello, Holberton, followed by a new line.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,15 @@
 global 	main
 	extern 	printf
 
-	section .text
+section .text
+
+section .data
+format:
+        db "Hello, Holberton", 10, 0
+
 main:
 	mov	rdi, format
 	mov	rax, 0
 	call	printf
 	mov	rax, 0
-	call    printf
-        pop     rbp
 	ret
-
-section .data
-format:
-	db "Hello, Holberton", 10, 0
-
